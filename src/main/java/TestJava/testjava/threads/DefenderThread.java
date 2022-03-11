2baebcc97d2bbf8fc1b82b2d61e187e409dc07f5
Edouard Dragon <edouard@glanum.com>
2022-03-11 11:09:40 +0100

    Fix: defenders targets and range

--- src/main/java/TestJava/testjava/threads/DefenderThread.java
+++ src/main/java/TestJava/testjava/threads/DefenderThread.java
@@ -4,12 +4,11 @@ import TestJava.testjava.classes.CustomEntity;
 import TestJava.testjava.helpers.CustomName;
 import TestJava.testjava.models.VillageModel;
 import TestJava.testjava.repositories.VillageRepository;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.Player;
-import org.bukkit.entity.Skeleton;
+import org.bukkit.entity.*;
 
 import java.util.Collection;
 import java.util.Objects;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class DefenderThread implements Runnable {
     @Override
@@ -17,25 +16,31 @@ public class DefenderThread implements Runnable {
         Collection<CustomEntity> entities = CustomName.getAll();
         entities = entities.stream().filter(entity -> entity.getEntity() instanceof Skeleton).toList();
         entities.forEach(entity -> {
-            Collection<Entity> targets = entity.getEntity().getNearbyEntities(10D, 10D, 10D);
+            AtomicBoolean haveTarget = new AtomicBoolean(false);
+            Collection<Entity> targets = entity.getEntity().getNearbyEntities(20D, 20D, 20D);
             targets.forEach(target -> {
-                if(!target.isCustomNameVisible() && !(target instanceof Player)) {
+                if (!target.isCustomNameVisible() && !(target instanceof Player)) {
                     return;
                 }
                 String entityVillage = CustomName.squareBrackets(entity.getEntity().getCustomName(), 0);
-                if(target instanceof Player) {
+                if (target instanceof Player) {
                     VillageModel village = VillageRepository.get(entityVillage);
-                    if(Objects.equals(village.getPlayerName(), ((Player) target).getDisplayName())) {
+                    if (Objects.equals(village.getPlayerName(), ((Player) target).getDisplayName())) {
                         return;
                     }
-                    entity.getEntity().attack(target);
+                    haveTarget.set(true);
+                    ((Mob) entity.getEntity()).setTarget((LivingEntity) target);
                     return;
                 }
                 String targetVillage = CustomName.squareBrackets(target.getCustomName(), 0);
-                if(!targetVillage.equals(entityVillage)) {
-                    entity.getEntity().attack(target);
+                if (!targetVillage.equals(entityVillage)) {
+                    haveTarget.set(true);
+                    ((Mob) entity.getEntity()).setTarget((LivingEntity) target);
                 }
             });
+            if (!haveTarget.get()) {
+                ((Mob) entity.getEntity()).setTarget(null);
+            }
         });
     }
 }
