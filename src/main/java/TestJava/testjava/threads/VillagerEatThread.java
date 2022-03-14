6d5c5d3ae6c0ccd1e1765659cbbe5e9b8da26cea
Edouard Dragon <edouard@glanum.com>
2022-03-14 14:28:48 +0100

    Feature: changement de village en cas de famine

--- src/main/java/TestJava/testjava/threads/VillagerEatThread.java
+++ src/main/java/TestJava/testjava/threads/VillagerEatThread.java
@@ -1,9 +1,13 @@
 package TestJava.testjava.threads;
 
 import TestJava.testjava.TestJava;
+import TestJava.testjava.classes.CustomEntity;
 import TestJava.testjava.helpers.Colorize;
+import TestJava.testjava.models.VillageModel;
 import TestJava.testjava.models.VillagerModel;
+import TestJava.testjava.repositories.VillageRepository;
 import TestJava.testjava.repositories.VillagerRepository;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Villager;
 
@@ -15,8 +19,9 @@ public class VillagerEatThread implements Runnable {
         Collection<VillagerModel> villagers = VillagerRepository.getAll();
         villagers.forEach(villagerModel -> {
             villagerModel.setFood(villagerModel.getFood() - 1);
+            Villager v = ((Villager) TestJava.plugin.getServer().getEntity(villagerModel.getId()));
+            VillageModel village = VillageRepository.get(villagerModel.getVillageName());
             if (villagerModel.getFood() <= 0) {
-                Villager v = ((Villager) TestJava.plugin.getServer().getEntity(villagerModel.getId()));
                 if (v == null) {
                     return;
                 }
@@ -24,6 +29,21 @@ public class VillagerEatThread implements Runnable {
                 TestJava.plugin.getServer().broadcastMessage(
                         ChatColor.GRAY + "La famine sévit à " + Colorize.name(villagerModel.getVillageName())
                 );
+            } else if (villagerModel.getFood() < 5) {
+                Collection<VillageModel> villages = VillageRepository.getAll();
+                VillageModel prosp = null;
+                for (VillageModel nVillage : villages) {
+                    if (prosp == null || (nVillage.getPopulation() > prosp.getPopulation() &&
+                            !nVillage.getId().equals(prosp.getId()))) {
+                        prosp = nVillage;
+                    }
+                }
+                CustomEntity ce = new CustomEntity(v);
+                villagerModel.setVillageName(prosp.getId());
+                Bukkit.getServer().broadcastMessage(Colorize.name(v.getCustomName()) + " s'est barré à "
+                        + Colorize.name(prosp.getId()) + " car il n'avait pas assez à manger");
+                ce.setVillage(prosp);
+                v.teleport(VillageRepository.getBellLocation(prosp));
             }
             VillagerRepository.update(villagerModel);
         });
