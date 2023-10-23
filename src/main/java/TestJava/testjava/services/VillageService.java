0688eef3a8345ee9ec1d775c528042e1c97456c0
IazLur <dragonedouard@hotmail.fr>
2023-10-23 20:46:32 +0200

    [v4] Frontières

--- src/main/java/TestJava/testjava/services/VillageService.java
+++ src/main/java/TestJava/testjava/services/VillageService.java
@@ -10,6 +10,8 @@ import TestJava.testjava.repositories.VillageRepository;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
@@ -18,6 +20,29 @@ import java.util.Collection;
 import java.util.UUID;
 
 public class VillageService {
+
+    private void drawCircle(Location center, int radius, Material material) {
+        World world = center.getWorld();
+        int centerX = center.getBlockX();
+        int centerZ = center.getBlockZ();
+
+        for (int x = -radius; x <= radius; x++) {
+            for (int z = -radius; z <= radius; z++) {
+                // Vérification si le point (x,z) est sur la circonférence du cercle
+                if ((x * x) + (z * z) <= radius * radius + 1 && (x * x) + (z * z) >= radius * radius - 1) {
+                    for (int y = 0; y < world.getMaxHeight(); y++) {
+                        Block block = world.getBlockAt(centerX + x, y, centerZ + z);
+                        Material blockType = block.getType();
+                        // Remplacer le bloc par de la bedrock sauf s'il s'agit d'air, d'eau ou de lave
+                        if (blockType != Material.AIR && blockType != Material.WATER && blockType != Material.LAVA) {
+                            block.setType(material);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
     public void create(BlockPlaceEvent e) {
         Location location = e.getBlockPlaced().getLocation();
         VillageModel village = new VillageModel();
@@ -32,6 +57,7 @@ public class VillageService {
                 " a créé " + Colorize.name(village.getId()));
         location.setY(location.getY() - 1);
         location.getBlock().setType(Material.BEDROCK);
+        drawCircle(location, Config.VILLAGE_PROTECTION_RADIUS, Material.BEDROCK);
     }
 
     public void testIfPlacingBed(BlockPlaceEvent e) {
